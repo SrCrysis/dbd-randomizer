7bad7ae96a3bdffbf9293dbe8a9ab2b7b8cc019d
SrCrysis <165341854+SrCrysis@users.noreply.github.com>
2026-06-29 03:52:20 -0400

    Change base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Cambia 'dbd-randomizer' por el nombre exacto de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
   base: '/dbd-randomizer/',
